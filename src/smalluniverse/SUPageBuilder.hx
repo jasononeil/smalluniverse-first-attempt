de9cd9df774452f77cd216f2221073aefdeab15b
Jason O'Neil <jason.oneil@gmail.com>
2017-04-29 10:55:25 +1000

    Empty get() method. Check and empty serverAction methods.
    - @:serverAction methods we check if they are fully typed, and return a promise
    - For now both of these are returning empty promises on the client, but this means they can compile without hassle.

--- src/smalluniverse/SUPageBuilder.hx
+++ src/smalluniverse/SUPageBuilder.hx
@@ -15,8 +15,12 @@ class SUPageBuilder {
 	static function processGetMethod(cb:ClassBuilder):Void {
 		for (member in cb) {
 			if (member.name == "get") {
-				// TODO: change client to make a HTTP call and return a promise of the same type.
 				#if client
+					var fn = member.getFunction().sure();
+					fn.expr = macro {
+						// TODO: make a HTTP call to return a promise of the same type.
+						return tink.core.Future.sync(Success(null));
+					}
 				#end
 				return;
 			}
@@ -27,12 +31,36 @@ class SUPageBuilder {
 		for (member in cb) {
 			switch member.extractMeta(':serverAction') {
 				case Success(_):
-					// TODO: check that all arguments are typed, and return type is a promise.
+					// Check that all arguments are typed, and return type is a promise.
+					var fn = member.getFunction().sure();
+					checkReturnTypeIsPromise(fn.ret, member.pos);
+					checkArgumentsAreExplicitlyTyped(fn.args, member.pos);
 
 					// TODO: transform the client to make a HTTP call and still return a promise of the same type.
 					#if client
+					fn.expr = macro {
+						// TODO: make a HTTP call to return a promise of the same type.
+						return tink.core.Future.sync(Success(null));
+					}
 					#end
 				default:
+			}
+		}
+	}
+
+	static function checkReturnTypeIsPromise(ret:ComplexType, pos:Position) {
+		var type = ret.toType(pos).sure();
+		switch Context.follow(type) {
+			case TAbstract(_.toString() => "tink.core.Promise", params):
+			case _:
+				Context.error('Function should return a tink.core.Promise', pos);
+		}
+	}
+
+	static function checkArgumentsAreExplicitlyTyped(args:Array<FunctionArg>, pos:Position) {
+		for (arg in args) {
+			if (arg.type == null) {
+				Context.warning('Argument ${arg.name} should have explicit type', pos);
 			}
 		}
 	}
