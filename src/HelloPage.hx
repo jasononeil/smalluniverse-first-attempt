335ef1d80f7fd7fbe2cb85bebd99543bc5d404ef
Jason O'Neil <jason.oneil@gmail.com>
2017-05-01 23:47:08 +1000

    Add an example which responds to keyups.
    I'm amazed this is working, pretty stoked :)

--- src/HelloPage.hx
+++ src/HelloPage.hx
@@ -6,6 +6,7 @@ import smalluniverse.SUMacro.jsx;
 	import haxe.Json;
 #elseif client
 	import js.Browser;
+	import js.html.*;
 #end
 using tink.CoreApi;
 
@@ -21,6 +22,7 @@ class HelloPage extends UniversalPage<{name:String, age:Int}, {}, {}> {
 			<h1 onClick=${clickHeader}>Hello ${this.props.name}</h1>
 			<h2><em>How does it feel being <strong>${""+this.props.age}</strong> years old?</em></h2>
 			<p>Nice to meet you! <b>:)</b></p>
+			<input onKeyUp=${keyup} />
 		</div>');
 	}
 
@@ -33,11 +35,26 @@ class HelloPage extends UniversalPage<{name:String, age:Int}, {}, {}> {
 		return Sys.getCwd();
 	}
 
+	@:serverAction public function changeName(name:String):Promise<Noise> {
+		var json = File.getContent('props.json');
+		var props:{name:String, age:Int} = Json.parse(json);
+		props.name = name;
+		json = Json.stringify(props);
+		File.saveContent('props.json', json);
+		return Noise;
+	}
+
 	@:client
 	function clickHeader() {
 		addExplanationMark().handle(function (outcome:Outcome<String,Error>) {
 			var path = outcome.sure();
 			Browser.alert('Executed at $path');
 		});
+	}
+
+	@:client
+	function keyup(e:react.ReactEvent) {
+		var target = cast (e.target, InputElement);
+		changeName(target.value);
 	}
 }
