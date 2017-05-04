aae6dd202737e804bdb57695fdf2c8e601bb0056
Jason O'Neil <jason.oneil@gmail.com>
2017-05-04 19:49:52 +1000

    Swap order of routes, as "/" is being treated as a wildcard so "about/" is never loaded.

--- src/Server.hx
+++ src/Server.hx
@@ -5,8 +5,8 @@ class Server {
 	static function main() {
 		var app = new Monsoon();
 		var smallUniverse = new SmallUniverse(app);
-		smallUniverse.addPage('/', HelloPage);
 		smallUniverse.addPage('/about', AboutPage);
+		smallUniverse.addPage('/', HelloPage);
 		app.listen(3000);
 	}
 }
