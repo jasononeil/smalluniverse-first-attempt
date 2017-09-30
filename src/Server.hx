01184fc2242fd8fb8888ad60d1d440e194235793
Jason O'Neil <jason.oneil@gmail.com>
2017-09-30 22:56:56 +1000

    Get a NodeJS version up and running

--- src/Server.hx
+++ src/Server.hx
@@ -1,9 +1,11 @@
 import monsoon.Monsoon;
+import monsoon.middleware.Static;
 import smalluniverse.SmallUniverse;
 
 class Server {
 	static function main() {
 		var app = new Monsoon();
+		app.use('/js', Static.serve('js'));
 		var smallUniverse = new SmallUniverse(app);
 		smallUniverse.addPage('/about', AboutPage);
 		smallUniverse.addPage('/:location?', HelloPage);
