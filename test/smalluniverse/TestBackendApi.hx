5549e15b86d8f22daa7f318845cca4d8f1a80bd6
Jason O'Neil <jason.oneil@gmail.com>
2017-11-04 16:22:30 +1100

    Write tests for BackendApi and it's macro

--- test/smalluniverse/TestBackendApi.hx
+++ test/smalluniverse/TestBackendApi.hx
@@ -2,12 +2,79 @@ package smalluniverse;
 
 import buddy.*;
 using buddy.Should;
+using tink.CoreApi;
+
+enum Action {
+	DoAThing;
+}
+typedef Props = {
+	name: String
+};
+
+/**
+To make things easier for developers, we try to make `BackendApi` classes exist on the client, so
+no tricky conditional compilation is needed to get your code compiling on both server and client.
+
+We do this by "emptying" the class using a build macro, getting rid of or emptying all methods, so
+that no server-side only things (like calls to `Sys.*`) exist on the client.
+
+This class tests that by putting a `Sys.programPath()` call in each method, and checking it compiles
+okay on the client.
+**/
+class SimpleBackendApi implements BackendApi<Action, Props> {
+	public function new() {
+		// Note: at this time I can't figure out how to empty the constructor using a macro,
+		// so we're not testing that use case.
+		// Sys.programPath();
+	}
+
+	public function get(context): Promise<Props> {
+		Sys.programPath();
+		someInstanceFn();
+		return {
+			name: "Jason"
+		};
+	}
+
+	public function processAction(context, action): Promise<BackendApiResult> {
+		Sys.programPath();
+		someStaticFn();
+		return BackendApiResult.Done;
+	}
+
+	function someInstanceFn() {
+		Sys.programPath();
+	}
+
+	static function someStaticFn() {
+		Sys.programPath();
+	}
+}
 
 class TestBackendApi extends BuddySuite {
 	public function new() {
 		describe("BackendApi", {
 			it("should compile an empty copy on the client without any hassle", {
+				var backendApi = new SimpleBackendApi();
 			});
+
+			#if server
+			it("should leave everything in tact and working on the server", function (done) {
+				var backendApi = new SimpleBackendApi(),
+					context = null;
+				backendApi
+					.get(context)
+					.next(function (props) {
+						props.name.should.be("Jason");
+						return backendApi.processAction(context, DoAThing);
+					})
+					.next(function (result) {
+						result.should.equal(BackendApiResult.Done);
+						return result;
+					})
+					.handle(done);
+			});
+			#end
 		});
 	}
 }
